3ce6fe0f0c111b46e260df20137c603eb1315867
Andre Ribau <ribauandre@yahoo.com>
2018-05-16 22:03:55 -0400

    deploy config

--- app.js
+++ app.js
@@ -15,11 +15,11 @@ var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':'false'}));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname,'public', 'index.html')));
 
 app.use('/api/book', book);
 
-// catch 404 and forward to error handler  //"homepage": "https://ribauandre.github.io/LibraryLender/",
+// catch 404 and forward to error handler  //
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
@@ -29,8 +29,7 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
 
   // render the error page
   res.status(err.status || 500);
