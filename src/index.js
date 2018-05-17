bce6404339c38f76fe39272db5d169c8a0018a87
Andre Ribau <ribauandre@yahoo.com>
2018-05-16 22:35:00 -0400

    deploy configs

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ import Create from './components/Create';
 ReactDOM.render(
   <Router>
       <div>
-        <Route exact path='/' component={App} />
-        <Route path='/edit/:id' component={Edit} />
-        <Route path='/create' component={Create} />
+        <Route exact path={process.env.PUBLIC_URL +'/'} component={App} />
+        <Route path={process.env.PUBLIC_URL+'/edit/:id'} component={Edit} />
+        <Route path={process.env.PUBLIC_URL+'/create'} component={Create} />
       </div>
   </Router>,
   document.getElementById('root')
