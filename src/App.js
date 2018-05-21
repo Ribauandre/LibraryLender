25cf6a61fa8a120b5b6bd56fd2b4135cf82cc1a7
ribauandre <ribauandre@yahoo.com>
2018-05-21 12:39:07 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -62,6 +62,12 @@ class App extends Component {
           </div>
         </div>
       </div>
+      <footer class="w3-container w3-grey w3-center w3-margin-top">
+      <p>Find me on linkedin, Github and <a href="https://ribauandre.com">RibauAndre.com</a> </p>
+      <a href="https://www.linkedin.com/in/ribauandre/" class="fa fa-linkedin w3-hover-opacity"></a>
+      <a href="https://github.com/Ribauandre" class="fa fa-github w3-hover-opacity"></a>
+  
+    </footer>
     );
   }
 }
